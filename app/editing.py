0d9dc2e25832163bd5724879288b38373f398fc8
stefaneduard-deaconu <stefaneduard.deaconu@gmail.com>
2020-07-14 20:44:56 +0300

    functions and TODOs before streaming video

---
+++ app/editing.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def syncToVideo(audio, video):
+    """:arg
+    """
+    # step 1
+    # extract audio from video and create the 2 numpy arrays
+    pass
+
+    # step 2
+    # overlay the two audio by using the windows between them
+    # we approximate the results with 4 frames per second
+    pass
+
+    # step 3
+    # create the video and stream it back
+    pass
