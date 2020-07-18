585f3e215eead4c3afc99700e3c87495c37c9a70
stefaneduard-deaconu <stefaneduard.deaconu@gmail.com>
2020-07-18 21:39:10 +0300

    finally. The app finds the most probable offset :D

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, File, UploadFile, Request
+from fastapi import FastAPI, Form, File, UploadFile, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
+from fastapi.responses import JSONResponse
+from typing import List
+import json
 from app.editing import syncToVideo
 
 app = FastAPI()
@@ -11,28 +13,25 @@ app.mount('/templates', StaticFiles(directory='templates'), name='templates')
 templates = Jinja2Templates(directory="templates")
 
 
-@app.post("/uploaded/")
-async def create_upload_files(request: Request, video: UploadFile = File(...), audio: UploadFile = File(...)):
+@app.post("/uploaded")
+async def create_upload_files(video: bytes = File(...), audio: bytes = File(...)):
     """
     :arg video is the video clip we edit by replacing the audio with the second argument, and syncing
     :arg audio is the better audio file we want to keep in the video
     """
 
-    content = [
-        {"video": video.filename,
-         'content-type': video.content_type},
+    # we call a function for checking the delay, then we return the offset for each (one of them is 0)
 
-        {'audio': audio.filename,
-         'content-type': audio.content_type}
-    ]
+    videoOffset, audioOffset = syncToVideo(video, audio)
 
-    # TODO create a streaming response
-
-    return templates.TemplateResponse('uploaded.html', {
-            'request': request,
-            "content": content,
-            "heading": "You can stream the video below"
-        })
+    return json.dumps({
+        'videoOffset': videoOffset,
+        'audioOffset': audioOffset
+    })
+    # return JSONResponse(status_code=200, content={
+    #     'videoOffset': videoOffset,
+    #     'audioOffset': audioOffset
+    # })
 
 
 @app.get("/")
@@ -42,7 +41,7 @@ async def root(request: Request):
 
     response = templates.TemplateResponse('home.html', {
         'request': request,
-        'greeting': 'Hello'
+        'greeting': 'Edit smartly'
     })
 
     return response
--- templates/scripts/home.js
+++ templates/scripts/home.js
@@ -1,6 +1,8 @@
 const toWav = require('audiobuffer-to-wav')
 window.toWav = toWav
 
+window.uploaded_url = '/uploaded'
+
 function switchVideo(element, file) {
     // remove all sources:
     Array.prototype.slice.call(video.children).forEach(child => video.removeChild(child))
@@ -13,9 +15,50 @@ function switchVideo(element, file) {
     element.appendChild(source);
 }
 
+function startPreview(offsets) {
+
+    let videoOffset = offsets.videoOffset
+    let audioOffset = offsets.audioOffset
+
+    window.video.classList.add('preview')
+    window.scrollTo(0,document.body.scrollHeight)
+}
+
 window.input_video = document.querySelector('input[name=video][type=file]')
 window.input_audio = document.querySelector('input[name=audio][type=file]')
 window.video = document.querySelector('video')
+window.audio = document.querySelector('audio')
+window.button = document.querySelector('button.submit')
+
+window.button.onclick = (e) => {
+    const url = '/uploaded'
+
+    var formData = new FormData();
+
+    console.log(window.audioWav)
+    var videoFile = new File([window.audioWav], "video.wav", {
+      type: "audio/x-wav",
+    });
+    formData.append('video', videoFile);
+    formData.append('audio', window.input_audio.files[0]);
+
+    const requestParams = {
+//        headers: {
+//            'Content-Type': 'multipart/form-data'//'audio/x-wav'
+//          TODO add an explicit boundary
+//        },
+        body: formData,
+        method: 'POST'
+    }
+
+    fetch(url, requestParams)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+        startPreview(data)
+    })
+    .catch(err=>console.log(err))
+}
 
 input_video.onchange = (e) => {
     file = e.target.files[0]
@@ -24,28 +67,28 @@ input_video.onchange = (e) => {
     var audioContext = new(window.AudioContext || window.webkitAudioContext)();
     let reader = new FileReader();
     var myBuffer;
-    const sampleRate = 16000;
-    const numberOfChannels = 1;
+    const sampleRate = 44100;
+    const numberOfChannels = 2;
 
 
     reader.onload = function () {
         var videoFileAsBuffer = reader.result; // arraybuffer
         audioContext.decodeAudioData(videoFileAsBuffer).then(function (decodedAudioData) {
-
+            console.log(decodedAudioData, decodedAudioData.duration)
             var duration = decodedAudioData.duration;
 
             var offlineAudioContext = new OfflineAudioContext(numberOfChannels, sampleRate * duration, sampleRate);
             var soundSource = offlineAudioContext.createBufferSource();
 
-              myBuffer = decodedAudioData;
-              soundSource.buffer = myBuffer;
-              soundSource.connect(offlineAudioContext.destination);
-              soundSource.start();
+             myBuffer = decodedAudioData;
+             soundSource.buffer = myBuffer;
+             soundSource.connect(offlineAudioContext.destination);
+             soundSource.start();
 
-              offlineAudioContext.startRendering().then(function (renderedBuffer) {
-                    window.audioData = renderedBuffer.getChannelData(0)
+             offlineAudioContext.startRendering().then(function (renderedBuffer) {
                     window.audioBuffer = renderedBuffer
                     window.audioWav = toWav(renderedBuffer)
+                    console.log(window.audioWav)
                   }).catch(function (err) {
                     console.log('Rendering failed: ' + err);
                   });
