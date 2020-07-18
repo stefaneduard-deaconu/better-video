fec2f10e27d5546f8f1c2c5e89b74e3bf8a8fb94
stefaneduard-deaconu <stefaneduard.deaconu@gmail.com>
2020-07-18 09:54:28 +0300

    we transformed the video's audio into wav

--- templates/scripts/home.js
+++ templates/scripts/home.js
@@ -1,62 +1,57 @@
 const toWav = require('audiobuffer-to-wav')
+window.toWav = toWav
 
 function switchVideo(element, file) {
-        // remove all sources:
+    // remove all sources:
+    Array.prototype.slice.call(video.children).forEach(child => video.removeChild(child))
+    // create and add the new source :D
+    var source = document.createElement('source');
 
-Array.prototype.slice.call(video.children).forEach(child => video.removeChild(child))
+    source.src = URL.createObjectURL(file)
+    source.type = file.type;
 
-            // create and add the new source :D
-            var source = document.createElement('source');
+    element.appendChild(source);
+}
 
-            source.src = URL.createObjectURL(file)
-            source.type = file.type;
+window.input_video = document.querySelector('input[name=video][type=file]')
+window.input_audio = document.querySelector('input[name=audio][type=file]')
+window.video = document.querySelector('video')
 
-            element.appendChild(source);
-        }
+input_video.onchange = (e) => {
+    file = e.target.files[0]
+    switchVideo(video, file)
 
-        var audioJSON
-
-
-        var input_video = document.querySelector('input[name=video][type=file]')
-        var input_audio = document.querySelector('input[name=audio][type=file]')
-        var video = document.querySelector('video')
-
-        input_video.onchange = (e) => {
-            file = e.target.files[0]
-            switchVideo(video, file)
-
-            var audioContext = new(window.AudioContext || window.webkitAudioContext)();
-            let reader = new FileReader();
-            var myBuffer;
-            const sampleRate = 16000;
-            const numberOfChannels = 1;
+    var audioContext = new(window.AudioContext || window.webkitAudioContext)();
+    let reader = new FileReader();
+    var myBuffer;
+    const sampleRate = 16000;
+    const numberOfChannels = 1;
 
 
-            reader.onload = function () {
-                var videoFileAsBuffer = reader.result; // arraybuffer
-                audioContext.decodeAudioData(videoFileAsBuffer).then(function (decodedAudioData) {
+    reader.onload = function () {
+        var videoFileAsBuffer = reader.result; // arraybuffer
+        audioContext.decodeAudioData(videoFileAsBuffer).then(function (decodedAudioData) {
 
-                    var duration = decodedAudioData.duration;
+            var duration = decodedAudioData.duration;
 
-                    var offlineAudioContext = new OfflineAudioContext(numberOfChannels, sampleRate * duration, sampleRate);
-                    var soundSource = offlineAudioContext.createBufferSource();
+            var offlineAudioContext = new OfflineAudioContext(numberOfChannels, sampleRate * duration, sampleRate);
+            var soundSource = offlineAudioContext.createBufferSource();
 
-                      myBuffer = decodedAudioData;
-                      soundSource.buffer = myBuffer;
-                      soundSource.connect(offlineAudioContext.destination);
-                      soundSource.start();
+              myBuffer = decodedAudioData;
+              soundSource.buffer = myBuffer;
+              soundSource.connect(offlineAudioContext.destination);
+              soundSource.start();
 
-                      offlineAudioContext.startRendering().then(function (renderedBuffer) {
-                            console.log(renderedBuffer); // outputs audiobuffer
-                            audioJSON = JSON.stringify(renderedBuffer.getChannelData(0))
-                            console.log(audioJSON.length)
-                            console.log(toWav(renderedBuffer.getChannelData(0)))
-                          }).catch(function (err) {
-                            console.log('Rendering failed: ' + err);
-                          });
-                });
-            }
+              offlineAudioContext.startRendering().then(function (renderedBuffer) {
+                    window.audioData = renderedBuffer.getChannelData(0)
+                    window.audioBuffer = renderedBuffer
+                    window.audioWav = toWav(renderedBuffer)
+                  }).catch(function (err) {
+                    console.log('Rendering failed: ' + err);
+                  });
+        });
+    }
 
 
-            reader.readAsArrayBuffer(file); // video file
-        }
+    reader.readAsArrayBuffer(file); // video file
+}
